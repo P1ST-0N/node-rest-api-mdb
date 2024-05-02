1084f587fb89287c560648365376cde7d3ea3367
P1ST-0N <luckylionia@gmail.com>
2024-05-02 11:52:58 +0300

    Update contact.js
    added contactSchema

--- models/contact.js
+++ models/contact.js
@@ -1,2 +1,27 @@
 import { Schema, model } from "mongoose";
-// import
+import handleMongooseError from "../helpers/handleMongooseError";
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+contactSchema.post("save", handleMongooseError);
+
+const Contact = model("contact", contactSchema);
+
+export default Contact;
